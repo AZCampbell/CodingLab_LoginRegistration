d3eca88549a409a76b2a5299e21230a7a34e9b4e
Jonathan Campbell <jpcampbell1127@gmail.com>
2024-05-07 20:49:18 -0700

    Update: Fixed autocomplete icloud / 1password showing on hidden fields using inert

--- SITE/resources/js/app.js
+++ SITE/resources/js/app.js
@@ -1,5 +1,18 @@
+// Declare variables for the form
+
 const formOpenButton = document.querySelector("#form-open");
 const home = document.querySelector(".home");
 const formContainer = document.querySelector(".form_container");
 const formCloseButton = document.querySelector(".form_close");
 const signupButton = document.querySelector("#signup");
+const loginButton = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenButton.addEventListener("click", (e) => {
+  home.classList.add("show");
+  home.removeAttribute("inert"); // WARNING: `this` is not `my_element`
+});
+formCloseButton.addEventListener("click", (e) => {
+  home.classList.remove("show");
+  home.setAttribute("inert", true);
+});
