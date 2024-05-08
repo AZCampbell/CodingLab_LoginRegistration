483074669a09e38a79951cd69866b1ee8652f951
Jonathan Campbell <jpcampbell1127@gmail.com>
2024-05-07 19:51:42 -0700

    Update: begin declaring const variables in JS file.

---
+++ SITE/resources/js/app.js
@@ -0,0 +1,5 @@
+const formOpenButton = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseButton = document.querySelector(".form_close");
+const signupButton = document.querySelector("#signup");
